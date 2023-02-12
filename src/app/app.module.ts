28eb10842dedf277883a6e605d89fbf5c906f44e
Mikolaj Rybarski <leczykponny@gmail.com>
2023-02-12 10:04:35 +0100

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadereComponent } from './headere/headere.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadereComponent
   ],
   imports: [
     BrowserModule,
