6bf89db036a4d8e6005453e520461e718e2385a2
Mikolaj Rybarski <leczykponny@gmail.com>
2023-02-14 15:45:17 +0100

    Added validationa and popup to auth

--- src/app/auth-component/auth-component.component.ts
+++ src/app/auth-component/auth-component.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,14 +13,31 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./auth-component.component.scss'],
 })
 export class AuthComponentComponent implements OnInit {
+  @ViewChild('popup') popUp: ElementRef;
   logInMode = true;
   authForm: FormGroup;
+
+  constructor(private rendere: Renderer2) {}
 
   ngOnInit() {
     this.authForm = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, Validators.required),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     });
+  }
+
+  onSwitchState() {
+    this.logInMode = !this.logInMode;
+    this.rendere.addClass(this.popUp.nativeElement, 'popupIn');
+    this.rendere.removeClass(this.popUp.nativeElement, 'popCover');
+
+    setTimeout(() => {
+      this.rendere.removeClass(this.popUp.nativeElement, 'popupIn');
+      this.rendere.addClass(this.popUp.nativeElement, 'popCover');
+    }, 2000);
   }
 
   onSubmit() {
